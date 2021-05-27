44b76dbc7c4acee14b6aed10b05e15ae31e5ce0a
edlcorrea <playered.lc@gmail.com>
2021-05-27 16:03:16 -0300

    Initial commit. Add initial folder structure.

---
+++ public/scripts/main.js
@@ -0,0 +1,5 @@
+function main() {
+  alert('Up and running.');
+}
+
+main();
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log('Started at port ' + port);
+});
