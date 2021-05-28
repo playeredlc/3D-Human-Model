e852646197fe62f7f9bf689ad0f32506b4549466
edlcorrea <playered.lc@gmail.com>
2021-05-28 19:37:18 -0300

    Add torso lateral flexion initial movement.

--- public/scripts/gui/torsoGui.js
+++ public/scripts/gui/torsoGui.js
@@ -6,11 +6,22 @@ const torsoSettings = {
     
     myModel.modelSkeleton.getBoneByName('spine001').rotation.x = amount;
   },
+
+  latExtAmount: 0,
+  latExtTorso: (amount) => {
+    myModel.modelSkeleton.getBoneByName('upper_armR').rotation.z = 2.35; // 135 deg
+    myModel.modelSkeleton.getBoneByName('upper_armL').rotation.z = -2.35;
+
+    myModel.modelSkeleton.getBoneByName('spine001').rotation.z = amount;
+    myModel.modelSkeleton.getBoneByName('spine002').rotation.z = amount;
+    myModel.modelSkeleton.getBoneByName('spine003').rotation.z = amount;
+  },
 }
 
 function initTorso(gui) {
   const torsoFolder = gui.addFolder('Torso');
   
-  torsoFolder.add(torsoSettings, 'extFlexAmount', -1, 1, 0.01).onChange(() => { torsoSettings.extFlexTorso(torsoSettings.extFlexAmount) });
+  torsoFolder.add(torsoSettings, 'extFlexAmount', -1, 1, 0.01).onChange(() => { torsoSettings.extFlexTorso(torsoSettings.extFlexAmount) }).name('Extension/Flexion');
+  torsoFolder.add(torsoSettings, 'latExtAmount', -0.26, 0.26, 0.01).onChange(() => { torsoSettings.latExtTorso(torsoSettings.latExtAmount) }).name('Lateral Extension');
 
 }
