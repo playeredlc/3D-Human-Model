e1c00e95d6885371271c7ed2ac254240fc24fd25
edlcorrea <playered.lc@gmail.com>
2021-05-28 19:15:20 -0300

    Add torso extension/flexion initial movement.

--- public/scripts/gui/torsoGui.js
+++ public/scripts/gui/torsoGui.js
@@ -1,8 +1,16 @@
 const torsoSettings = {
-  moveTorso: () => {},
+  extFlexAmount: 0,
+  extFlexTorso: (amount) => {
+    myModel.modelSkeleton.getBoneByName('upper_armR').rotation.z = Math.PI;
+    myModel.modelSkeleton.getBoneByName('upper_armL').rotation.z = Math.PI;
+    
+    myModel.modelSkeleton.getBoneByName('spine001').rotation.x = amount;
+  },
 }
 
 function initTorso(gui) {
   const torsoFolder = gui.addFolder('Torso');
   
+  torsoFolder.add(torsoSettings, 'extFlexAmount', -1, 1, 0.01).onChange(() => { torsoSettings.extFlexTorso(torsoSettings.extFlexAmount) });
+
 }
