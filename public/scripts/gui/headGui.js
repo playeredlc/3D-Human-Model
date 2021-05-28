bc3986e369dbc138df0101a3ceb815d671b4ed84
edlcorrea <playered.lc@gmail.com>
2021-05-28 16:27:10 -0300

    Add head lateral flexion initial movement.

--- public/scripts/gui/headGui.js
+++ public/scripts/gui/headGui.js
@@ -3,11 +3,17 @@ const headSettings = {
   extFlexHead: (amount) => {
   myModel.modelSkeleton.getBoneByName('spine004').rotation.x = amount;  
   },
+
+  latFlexAmount: 0,
+  latFlexHead: (amount) => {
+    myModel.modelSkeleton.getBoneByName('spine004').rotation.z = amount;
+    myModel.modelSkeleton.getBoneByName('spine005').rotation.z = amount * 2;
+  },
 }
 
 function initHead(gui) {
   const headFolder = gui.addFolder('Head');
 
   headFolder.add(headSettings, 'extFlexAmount', 0, 1.8, 0.01).onChange(() => { headSettings.extFlexHead(headSettings.extFlexAmount) }).name('Extension/Flexion');
-
+  headFolder.add(headSettings, 'latFlexAmount', -0.3, 0.3, 0.01).onChange(() => {headSettings.latFlexHead(headSettings.latFlexAmount) }).name('Lateral Flexion');
 }
