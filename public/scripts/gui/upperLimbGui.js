f17fcdf84a61c7786e937fe0a716b343d947391e
edlcorrea <playered.lc@gmail.com>
2021-05-28 17:21:18 -0300

    Add right arm adduction/abduction initial movement.

--- public/scripts/gui/upperLimbGui.js
+++ public/scripts/gui/upperLimbGui.js
@@ -1,8 +1,13 @@
 const upperSettings = {
-  moveArm: () => {},
+  abductionAmount: 1.57,
+  abductionUpper: (amount) => {
+    myModel.modelSkeleton.getBoneByName('upper_armR').rotation.z = amount;
+  },
 }
 
 function initUpperLimb(gui) {
   const upperFolder = gui.addFolder('Upper Limb');
+
+  upperFolder.add(upperSettings, 'abductionAmount', 0.33, Math.PI, 0.01).onChange(() => { upperSettings.abductionUpper(upperSettings.abductionAmount) }).name('Adduction/Abduction');
 
 }
