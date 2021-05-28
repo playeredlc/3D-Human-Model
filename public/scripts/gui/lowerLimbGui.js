b3a82c8483c2843b02b3e142bb68509b9a793afd
edlcorrea <playered.lc@gmail.com>
2021-05-28 20:12:02 -0300

    Add hip extension/flexion initial movement.

--- public/scripts/gui/lowerLimbGui.js
+++ public/scripts/gui/lowerLimbGui.js
@@ -1,9 +1,16 @@
 const lowerSettings = {
-  moveLeg: () => {},
+  hip: {
+    extFlexAmount: Math.PI,
+    extFlexHip: () => {
+      myModel.modelSkeleton.getBoneByName('thighR').rotation.x = lowerSettings.hip.extFlexAmount;
+    },
+  },
 }
 
 
 function initLowerLimb(gui) {
   const lowerFolder = gui.addFolder('Lower Limb');
+
+  lowerFolder.add(lowerSettings.hip, 'extFlexAmount', Math.PI/2, Math.PI + (0.26), 0.01).onChange(() => { lowerSettings.hip.extFlexHip() }).name('Hip Ext/Flex');
   
 }
