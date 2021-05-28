67761c459d588a7197b6c721db8861c949fd680c
edlcorrea <playered.lc@gmail.com>
2021-05-28 16:28:33 -0300

    Add head rotation initial movement.

--- public/scripts/gui/headGui.js
+++ public/scripts/gui/headGui.js
@@ -9,6 +9,13 @@ const headSettings = {
     myModel.modelSkeleton.getBoneByName('spine004').rotation.z = amount;
     myModel.modelSkeleton.getBoneByName('spine005').rotation.z = amount * 2;
   },
+
+  rotationAmount: 0,
+  rotateHead: (amount) => {
+    myModel.modelSkeleton.getBoneByName('spine004').rotation.y = amount;
+    myModel.modelSkeleton.getBoneByName('spine005').rotation.y = amount;
+    myModel.modelSkeleton.getBoneByName('spine006').rotation.y = amount;
+  },
 }
 
 function initHead(gui) {
@@ -16,4 +23,6 @@ function initHead(gui) {
 
   headFolder.add(headSettings, 'extFlexAmount', 0, 1.8, 0.01).onChange(() => { headSettings.extFlexHead(headSettings.extFlexAmount) }).name('Extension/Flexion');
   headFolder.add(headSettings, 'latFlexAmount', -0.3, 0.3, 0.01).onChange(() => {headSettings.latFlexHead(headSettings.latFlexAmount) }).name('Lateral Flexion');
+  headFolder.add(headSettings, 'rotationAmount', -0.3, 0.3, 0.01).onChange(() => { headSettings.rotateHead(headSettings.rotationAmount) }).name('Rotation');
+  
 }
