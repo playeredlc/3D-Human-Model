7002c96ec2d9a8a27dee619d35a6ae9dd716a11f
edlcorrea <playered.lc@gmail.com>
2021-05-28 21:03:10 -0300

    Add knee extension/flexion initial movement.

--- public/scripts/gui/lowerLimbGui.js
+++ public/scripts/gui/lowerLimbGui.js
@@ -5,6 +5,16 @@ const lowerSettings = {
       myModel.modelSkeleton.getBoneByName('thighR').rotation.x = lowerSettings.hip.extFlexAmount;
     },
   },
+  
+  knee: {
+    extFlexAmount: Math.PI/2,
+    extFlexKnee: () => {
+      myModel.modelSkeleton.getBoneByName('thighR').rotation.x = Math.PI/2;
+      myModel.modelSkeleton.getBoneByName('shinR').rotation.x = Math.PI/2;
+
+      myModel.modelSkeleton.getBoneByName('shinR').rotation.x = lowerSettings.knee.extFlexAmount;
+    },
+  }
 }
 
 
@@ -12,5 +22,6 @@ function initLowerLimb(gui) {
   const lowerFolder = gui.addFolder('Lower Limb');
 
   lowerFolder.add(lowerSettings.hip, 'extFlexAmount', Math.PI/2, Math.PI + (0.26), 0.01).onChange(() => { lowerSettings.hip.extFlexHip() }).name('Hip Ext/Flex');
+  lowerFolder.add(lowerSettings.knee, 'extFlexAmount', 0, (Math.PI/2 + 1.13), 0.01).onChange(() => { lowerSettings.knee.extFlexKnee() }).name('Knee Ext/Flex');
   
 }
