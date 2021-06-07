b936e67e26ecee153323c2460163fb1892ea62b4
edlcorrea <playered.lc@gmail.com>
2021-06-06 23:04:48 -0300

    Add torso rotaton initial movement.

--- public/scripts/gui/torsoGui.js
+++ public/scripts/gui/torsoGui.js
@@ -4,7 +4,9 @@ const torsoSettings = {
     myModel.modelSkeleton.getBoneByName('upper_armR').rotation.z = Math.PI;
     myModel.modelSkeleton.getBoneByName('upper_armL').rotation.z = Math.PI;
     myModel.modelSkeleton.getBoneByName('forearmR').rotation.x = 0.16; // 10 deg
+    myModel.modelSkeleton.getBoneByName('forearmL').rotation.x = 0.16; // 10 deg
     myModel.modelSkeleton.getBoneByName('forearmR').rotation.z = -0.105; // 6 deg
+    myModel.modelSkeleton.getBoneByName('forearmL').rotation.z = 0.105; // 6 deg    
     
     myModel.modelSkeleton.getBoneByName('spine001').rotation.x = torsoSettings.extFlexAmount;
   },
@@ -14,11 +16,24 @@ const torsoSettings = {
     myModel.modelSkeleton.getBoneByName('upper_armR').rotation.z = 2.35; // 135 deg
     myModel.modelSkeleton.getBoneByName('upper_armL').rotation.z = -2.35;
     myModel.modelSkeleton.getBoneByName('forearmR').rotation.x = 0.16; // 10 deg
+    myModel.modelSkeleton.getBoneByName('forearmL').rotation.x = 0.16; // 10 deg
     myModel.modelSkeleton.getBoneByName('forearmR').rotation.z = -0.105; // 6 deg
 
     myModel.modelSkeleton.getBoneByName('spine001').rotation.z = torsoSettings.latExtAmount;
     myModel.modelSkeleton.getBoneByName('spine002').rotation.z = torsoSettings.latExtAmount;
     myModel.modelSkeleton.getBoneByName('spine003').rotation.z = torsoSettings.latExtAmount;
+  },
+
+  rotationAmount: 0,
+  rotationTorso: () => {
+    myModel.modelSkeleton.getBoneByName('upper_armR').rotation.z = 2.35; // 135 deg
+    myModel.modelSkeleton.getBoneByName('upper_armL').rotation.z = -2.35;
+    myModel.modelSkeleton.getBoneByName('forearmR').rotation.x = Math.PI / 2; // 10 deg
+    myModel.modelSkeleton.getBoneByName('forearmL').rotation.x = Math.PI / 2; // 10 deg
+
+    myModel.modelSkeleton.getBoneByName('spine001').rotation.y = torsoSettings.rotationAmount;
+    myModel.modelSkeleton.getBoneByName('spine002').rotation.y = torsoSettings.rotationAmount;
+    myModel.modelSkeleton.getBoneByName('spine003').rotation.y = torsoSettings.rotationAmount;
   },
 }
 
@@ -27,5 +42,5 @@ function initTorso(gui) {
   
   torsoFolder.add(torsoSettings, 'extFlexAmount', -1, 1, 0.01).onChange(() => { torsoSettings.extFlexTorso() }).name('Extension/Flexion');
   torsoFolder.add(torsoSettings, 'latExtAmount', -0.26, 0.26, 0.01).onChange(() => { torsoSettings.latExtTorso() }).name('Lateral Flexion');
-
+  torsoFolder.add(torsoSettings, 'rotationAmount', -0.34, 0.34, 0.01).onChange(() => { torsoSettings.rotationTorso() });
 }
