1cf29716b641747db61447ecdf6e35496dd65f7e
edlcorrea <playered.lc@gmail.com>
2022-03-06 21:01:10 -0300

    Add all upper limb animations.

--- src/scripts/animations/upperLimbAnimations.js
+++ src/scripts/animations/upperLimbAnimations.js
@@ -6,17 +6,105 @@ var animation = {
     rExtensionFlexion: () => {
       const lowerBound = 0.33;
       const upperBound = Math.PI;
-      const stepSize = 0.02;
       
       animation.move(
         lowerBound,
         upperBound,
-        stepSize,
         upperSettings.right,
         'extFlexAmount',
         upperSettings.right.extFlexUpper
       );
+    },
 
+    lExtensionFlexion: () => {
+      const lowerBound = 0.33;
+      const upperBound = Math.PI;
+      
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.left,
+        'extFlexAmount',
+        upperSettings.left.extFlexUpper
+      );
+    },    
+
+    rAdductionAbduction: () => {
+      const lowerBound = 0.33;
+      const upperBound = Math.PI;
+
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.right,
+        'abductionAmount',
+        upperSettings.right.abductionUpper,
+      );
+    },
+
+    lAdductionAbduction: () => {
+      const lowerBound = 0.33;
+      const upperBound = Math.PI;
+
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.left,
+        'abductionAmount',
+        upperSettings.left.abductionUpper,
+      );
+    },
+
+    rHorizontalAdductionAbduction: () => {
+      const lowerBound = -1;
+      const upperBound = 0.61;
+
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.right,
+        'horizAbductionAmount',
+        upperSettings.right.horizAbductionUpper,
+      );
+    },
+
+    lHorizontalAdductionAbduction: () => {
+      const lowerBound = -1;
+      const upperBound = 0.61;
+
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.left,
+        'horizAbductionAmount',
+        upperSettings.left.horizAbductionUpper,
+      );
+    },
+
+    rLateralMedialRotation: () => {
+      const lowerBound = (-Math.PI/2);
+      const upperBound = 1.30;
+
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.right,
+        'latRotationAmount',
+        upperSettings.right.latRotationUpper,
+      );
+    },
+
+    lLateralMedialRotation: () => {
+      const lowerBound = (-Math.PI/2);
+      const upperBound = 1.30;
+
+      animation.move(
+        lowerBound,
+        upperBound,
+        upperSettings.left,
+        'latRotationAmount',
+        upperSettings.left.latRotationUpper,
+      );
     },
   },
 };
